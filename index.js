cd8d903958270ba048c666a350a6a0b46d0f3222
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-23 22:37:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function mapToNegativize(sourceArray) {
+<<<<<<< HEAD
 
+=======
+  let newArray = sourceArray.map(* -1);
+  return newArray;
+>>>>>>> fb4daaaa278b4cac653f4123869f5df90d745798
 };
 
 function mapToNoChange(sourceArray) {
